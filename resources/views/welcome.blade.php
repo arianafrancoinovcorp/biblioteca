eb692b6c1e5672d06e7030f1fde577bd68b2ddb5
arianafrancoinovcorp <ariana.franco@trainee.inovcorp.com>
2025-07-28 22:07:25 +0100

    Header Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Mundo das Histórias</title>
+    <title>Beyond the page</title>
 
     <link rel="icon" href="/favicon.ico" sizes="any" />
     <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
@@ -15,19 +16,37 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     <!-- Styles -  adicionar o Tailwind/DaisyUI depois -->
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
-<body class="bg-[#FDFDFC] text-[#1b1b18] flex flex-col items-center justify-center min-h-screen p-6 lg:p-8">
-    @if (Route::has('login'))
-    <nav class="w-full max-w-4xl flex justify-end gap-4 max-w-[335px] mb-6">
-        @auth
-            <a href="{{ url('/dashboard') }}" class="border border-[#19140035] rounded-sm px-5 py-1.5 text-sm hover:border-[#1915014a]">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}" class="text-[#1b1b18] border border-transparent rounded-sm px-5 py-1.5 text-sm hover:border-[#19140035]">Log in</a>
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="border border-[#19140035] rounded-sm px-5 py-1.5 text-sm hover:border-[#1915014a]">Register</a>
-            @endif
-        @endauth
-    </nav>
-    @endif
+
+<body data-theme="light" class="bg-[#FDFDFC] flex flex-col min-h-screen ">
+<nav class="navbar bg-[] px-4">
+    <div class="flex-1">
+        <a href="/" class="text-xl font-semibold normal-case">
+            Beyond the Page
+        </a>
+    </div>
+    <div class="flex-none space-x-2">
+        @if (Route::has('login'))
+            @auth
+                <a href="{{ url('/dashboard') }}">
+                    <button class="btn btn-primary btn-outline">Dashboard</button>
+                </a>
+            @else
+                <a href="{{ route('login') }}">
+                    <button class="btn btn-primary btn-outline">Log in</button>
+                </a>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">
+                        <button class="btn btn-primary">Register</button>
+                    </a>
+                @endif
+            @endauth
+        @endif
+    </div>
+</nav>
 </body>
+
 </html>
