b4d614bbe38ba3b83f7aad6f603bb82615ec9a03
arianafrancoinovcorp <ariana.franco@trainee.inovcorp.com>
2025-10-04 01:15:58 +0100

    Resolve merge conflicts

--- routes/web.php
+++ routes/web.php
@@ -97,13 +97,10 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/checkout', [CheckoutController::class, 'showForm'])->name('checkout.form');
     Route::post('/checkout', [CheckoutController::class, 'process'])->name('checkout.process');
-    Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
-    
+    Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success'); 
 });
 
-
 require __DIR__ . '/auth.php';
-
 
 // --------------------
 // Test
